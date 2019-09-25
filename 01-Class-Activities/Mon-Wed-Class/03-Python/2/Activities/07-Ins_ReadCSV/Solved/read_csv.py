ec898f04fb236f8f819c4fde1b7fa1e4200afbe9
= <=>
2019-09-25 17:15:53 -0400

    Updating Unit 3.2 activities

--- 01-Class-Activities/Mon-Wed-Class/03-Python/2/Activities/07-Ins_ReadCSV/Solved/read_csv.py
+++ 01-Class-Activities/Mon-Wed-Class/03-Python/2/Activities/07-Ins_ReadCSV/Solved/read_csv.py
@@ -7,16 +7,7 @@ import csv
 
 csvpath = os.path.join('..', 'Resources', 'accounting.csv')
 
-# # Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
-
-# Method 2: Improved Reading using CSV module
-
-with open(csvpath, newline='') as csvfile:
+with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -25,7 +16,7 @@ with open(csvpath, newline='') as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    print("CSV Header: " + str(csv_header))
 
     # Read each row of data after the header
     for row in csvreader:
