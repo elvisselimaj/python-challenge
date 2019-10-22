89d45a493cd822f10b4c479fb2184c6afd45d4bf
Williams, Dartanion De Angelo <dartanion@dartanion.net>
2019-10-21 22:36:44 -0400

    Updating Unit 7 materials

---
+++ 01-Class-Activities/Mon-Wed-Class/07-Social-Analytics/1/Activities/01-Ins_Get_Home_Tweets/Solved/hussein.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "kWB5UzKVaeWnYk2SXCL9PylnF"
+consumer_secret = "KUn0jkFqHYIhcPoHlQNM4UT1UWZ93TAa6rlwTv2Fd4ZeezStTf"
+access_token = "1182156927113019393-6idqEdFvUHs0D4fTnKXeEZ7bX7XB4u"
+access_token_secret = "k8B5bpwFxTDTEEY10eZT3A21Zx7kwq2WoDjc1u8U87adS"
